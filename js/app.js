2007b4530d40d9a4fa071af937db5ff5bccb573b
chepiga1995 <ura1995ura@gmail.com>
2015-10-20 06:00:50 +0300

    code refactoring

--- js/app.js
+++ js/app.js
@@ -1,54 +1,53 @@
-// Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+var Entity = function(x, y, speed){
     this.x = x;
     this.y = y;
     this.speed = speed;
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    this.x += dt * this.speed;
-    this.x = this.x % 505;
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+}
+Entity.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+//Enemy object
+var Enemy = function() {
+    Entity.apply(this, arguments);
+    this.sprite = 'images/enemy-bug.png';
+};
 
+Enemy.prototype = Object.create(Entity.prototype);
+Enemy.prototype.constructor = Enemy;
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+//Enemy's methods
+
+Enemy.prototype.update = function(dt) {
+    this.x += dt * this.speed;
+    this.x = this.x % 505;
+};
+
+//Player
 
 var Player = function(){
-    Enemy.apply(this, arguments);
+    Entity.apply(this, arguments);
     this.sprite = 'images/char-boy.png';
 }
-Player.prototype = Object.create(Enemy.prototype);
+
+Player.prototype = Object.create(Entity.prototype);
 Player.prototype.constructor = Player;
-var allEnemies = [new Enemy(20, 20, 50)];
-var player = new Player(120, 120, 50);
+
+//Player's methods
+
 Player.prototype.handleInput = function(direction){
     this.x += this.speed * direction[0];
     this.y += this.speed * direction[1];
 };
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+
+//Instance
+
+var allEnemies = [new Enemy(20, 20, 50)];
+var player = new Player(120, 120, 50);
+
+
+
+
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: [-1, 0],
